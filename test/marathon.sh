451bf26039b9b828124f8aee64dcb5320adf10d3
Karl Isenberg <karlkfi@yahoo.com>
2016-03-17 12:54:09 -0700

    Mesos 0.27.2 & Marathon 0.15.3
    - updated marathon test to use bridged networkign and clean up after itself

--- test/marathon.sh
+++ test/marathon.sh
@@ -19,19 +19,30 @@ wget -SO - \
     --post-file="${project_root}/test/assets/nginx/marathon.json" \
     http://${marathon_ip}:8080/v2/apps
 
-function test::nginx_host {
-  wget -SO - "http://${marathon_ip}:8080/v2/apps/nginx?embed=apps.counts" | jq '.app.tasks[0].host' --raw-output
+function test::nginx_address {
+  wget -SO - "http://${marathon_ip}:8080/v2/apps/nginx?embed=apps.counts" | jq '.app.tasks[0] | "\(.host):\(.ports[0])"' --raw-output
 }
 
 # marathon updates the apps data pretty quickly, but not before returning from the creation request
 for i in {1..30}; do
-  host="$(test::nginx_host)"
-  if [ "${host}" != "null" ]; then
+  app_address="$(test::nginx_address)"
+  if [ "${app_address}" != "null:null" ]; then
     break
   fi
   sleep 2
 done
-#host="nginx.marathon.mesos"
+#app_address="nginx.marathon.mesos"
+if [ "${app_address}" == "null" ]; then
+  echo "Timed out waiting for Nginx App to deploy!" 1>&2
+  exit 2
+fi
+
+echo "Nginx App: http://${app_address}/" 1>&2
 
 # once app is created, it may take a while to be ready (esp if downloading the docker image)
-probe_until_ready "http://${host}:80/" 120
+probe_until_ready "http://${app_address}/" 120
+
+echo "Deleting Nginx App" 1>&2
+wget -SO - --method=DELETE http://${marathon_ip}:8080/v2/apps/nginx
+
+echo "Test Passed!" 1>&2
